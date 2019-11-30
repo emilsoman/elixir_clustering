19808fd4808a87e7795170b4ed0cfe9d0005ed9f
Emil Soman <emil.soman@gmail.com>
2019-12-01 03:22:18 +0530

    Initial commit

---
+++ mix.exs
@@ -0,0 +1,29 @@
+defmodule MyApp.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :my_app,
+      version: "0.1.0",
+      elixir: "~> 1.9",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {MyApp.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:libcluster, "~> 3.1"},
+      {:swarm, "~> 3.4"}
+    ]
+  end
+end
